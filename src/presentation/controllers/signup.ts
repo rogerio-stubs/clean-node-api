df2f57358f9ffccf48b0a4a5b61d26dd8769e263
rogerio-stubs <rostubs17@gmail.com>
2022-11-11 08:36:53 -0300

    refactor: move duplicated code to a helper method

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,21 +1,16 @@
 import { MissingParamError } from '../errors/missing-param-error'
+import { badRequest } from '../helpers/http-helper'
 import { HttpRequest } from '../protocols/http'
 export class SignUpController {
   handle (httpRequest: HttpRequest): any {
     // eslint-disable-next-line no-useless-catch
     try {
       if (!httpRequest.body.name) {
-        return {
-          statusCode: 400,
-          body: new MissingParamError('name')
-        }
+        return badRequest(new MissingParamError('name'))
       }
 
       if (!httpRequest.body.email) {
-        return {
-          statusCode: 400,
-          body: new MissingParamError('email')
-        }
+        return badRequest(new MissingParamError('email'))
       }
     } catch (error) {
       throw (error)
