e9b5f7fb72fc3b4d215dc9accdbefc82d6626701
rogerio-stubs <rostubs17@gmail.com>
2022-11-17 09:33:04 -0300

    feat: ensure SignUpController returns 500 if EmailValidator throws

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,5 +1,6 @@
 import { InvalidParamError } from '../errors/invalid-param-error'
 import { MissingParamError } from '../errors/missing-param-error'
+import { ServerError } from '../errors/server-error'
 import { badRequest } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
 import { EmailValidator } from '../protocols/email-validator'
@@ -26,7 +27,10 @@ export class SignUpController implements Controller {
         return badRequest(new InvalidParamError('email'))
       }
     } catch (error) {
-      throw (error)
+      return {
+        statusCode: 500,
+        body: new ServerError()
+      }
     }
   }
 }
